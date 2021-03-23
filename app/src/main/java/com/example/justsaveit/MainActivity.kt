f6d0003b871a608aa86ddf739d605a69726ecaeb
dededarirahmadi <dededarirahmadi@gmail.com>
2021-03-24 04:47:20 +0700

    fix darkmode

--- app/src/main/java/com/example/justsaveit/MainActivity.kt
+++ app/src/main/java/com/example/justsaveit/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.justsaveit
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import me.ibrahimsn.lib.SmoothBottomBar
 
@@ -15,18 +19,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var navbar:SmoothBottomBar
+    private lateinit var sharedPref:SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        val isNightMode = sharedPref.getBoolean("nightMode", false)
+        if (isNightMode){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
 
-//        val currentNightMode = Configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-//        when (currentNightMode) {
-//            Configuration.UI_MODE_NIGHT_NO -> {} // Night mode is not active, we're using the light theme
-//            Configuration.UI_MODE_NIGHT_YES -> {} // Night mode is active, we're using dark theme
-//        }
 
         navbar = findViewById(R.id.nav_bar)
 
@@ -37,11 +43,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_bottom_nav,menu)
         navbar.setupWithNavController(menu!!,navController)
-        return true
+        menu.add(Menu.NONE, 1 , Menu.NONE, R.string.menu_dark_mode)
+        menu.add(Menu.NONE, 2 , Menu.NONE, R.string.menu_light_mode)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            1 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                with(sharedPref.edit()){
+                    putBoolean("nightMode", true)
+                    apply()
+                }
+            }
+            2 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                with(sharedPref.edit()){
+                    putBoolean("nightMode", false)
+                    apply()
+                }
+            }
+            else -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                with(sharedPref.edit()){
+                    putBoolean("nightMode", false)
+                    apply()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         navController.navigateUp()
         return true
     }
+
+
 }
--- app/src/main/java/com/example/justsaveit/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/justsaveit/ui/splash/SplashActivity.kt
@@ -1,17 +1,31 @@
 package com.example.justsaveit.ui.splash
 
 import android.animation.Animator
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.airbnb.lottie.LottieAnimationView
 import com.example.justsaveit.MainActivity
 import com.example.justsaveit.R
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var sharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        val isNightMode = sharedPref.getBoolean("nightMode", false)
+        if (isNightMode){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
 
         val animSplash = findViewById<LottieAnimationView>(R.id.anim_splash)
 
