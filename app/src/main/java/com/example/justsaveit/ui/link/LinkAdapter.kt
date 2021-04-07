b9d0b3896a02eeda1a6164b883474ccfaf005576
deded <dededarirahmadi@gmail.com>
2021-04-07 23:51:25 +0700

    edit note & link adapter

--- app/src/main/java/com/example/justsaveit/ui/link/LinkAdapter.kt
+++ app/src/main/java/com/example/justsaveit/ui/link/LinkAdapter.kt
@@ -1,5 +1,33 @@
 package com.example.justsaveit.ui.link
 
-class LinkAdapter (){
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.justsaveit.R
+import com.example.justsaveit.databinding.ItemLinkBinding
+import com.example.justsaveit.domain.model.Link
+
+class LinkAdapter : RecyclerView.Adapter<LinkAdapter.LinkViewHolder> (){
+    private var listData = ArrayList<Link>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LinkAdapter.LinkViewHolder =
+            LinkViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_link, parent, false))
+
+    override fun onBindViewHolder(holder: LinkAdapter.LinkViewHolder, position: Int) {
+        val data = listData[position]
+        holder.bind(data)
+    }
+
+    override fun getItemCount() = listData.size
+
+    inner class LinkViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val binding =ItemLinkBinding.bind(itemView)
+        fun bind(data: Link){
+            with(binding){
+
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/example/justsaveit/ui/note/NoteAdapter.kt
+++ app/src/main/java/com/example/justsaveit/ui/note/NoteAdapter.kt
@@ -1,5 +1,33 @@
 package com.example.justsaveit.ui.note
 
-class NoteAdapter () {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.justsaveit.R
+import com.example.justsaveit.databinding.ItemNoteBinding
+import com.example.justsaveit.domain.model.Note
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+    private var listData = ArrayList<Note>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NoteViewHolder =
+            NoteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false))
+
+    override fun onBindViewHolder(holder: NoteAdapter.NoteViewHolder, position: Int) {
+        val data = listData[position]
+        holder.bind(data)
+    }
+
+    override fun getItemCount() = listData.size
+
+    inner class NoteViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        private val binding = ItemNoteBinding.bind(itemView)
+        fun bind(data: Note){
+            with(binding){
+
+            }
+        }
+    }
 
 }
